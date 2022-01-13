86bf9049db4785858ee94edb17fe97d8bedc7b4c
Harel Afriat <harelaf@post.bgu.ac.il>
2022-01-13 23:46:42 +0200

    Temporary testing files.

--- foo.scm
+++ foo.scm
@@ -1,2 +1,8 @@
-(define foo (lambda (x) (lambda (y) x)))
-((foo 1) 2)
+(letrec ((loop (lambda (x n)
+                   (if (zero? n)
+                       #t
+                       (begin
+                         (set! x 5)
+                         (lambda () x)
+                         (loop x (- n 1)))))))
+    (loop #f 4000000))
